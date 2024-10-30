efd06ab55ac0198dc6b04db433bd0ddd5bce78ff
Luís Fernando Miki <69601772+luisfmiki@users.noreply.github.com>
2024-10-30 17:47:00 -0300

    Delete build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_spectral_calibration_FILE D:/miki/software/spectral_calibration/spectral_calibration/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/spectral_calibration.exe)
-set(__QT_DEPLOY_TARGET_spectral_calibration_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_spectral_calibration_RUNTIME_DLLS D:/miki/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/miki/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/miki/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
